b9f4ec79bc138238ac85db6f5ef0110117bc7362
Hepin <hepinponkiya@gmail.com>
2024-04-12 15:58:33 +0530

    DIL Result Page Done

--- src/app/DilComponent/result-card/result-card.component.ts
+++ src/app/DilComponent/result-card/result-card.component.ts
@@ -1,32 +1,47 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FilteredService } from '../Services/filtered.service';
+import { MilestoneService } from '../Services/milestone.service';
 @Component({
   selector: 'app-result-card',
   templateUrl: './result-card.component.html',
-  styleUrls: ['./result-card.component.scss']
+  styleUrls: ['./result-card.component.scss'],
 })
 export class ResultCardComponent {
-  dils: any = {}; // Change type to any for object
+  dils: any; // Change type to any for object
   // @Output() milestoneID: EventEmitter<number> = new EventEmitter<number>();
 
   @Input() loanID: number = 0;
   @Input() investorID: number = 0;
   @Input() clientID: number = 0;
   @Input() milestoneID: number = 0;
-  constructor(private filteredService:FilteredService ) { }
-  selectForeClosure(resultData: string) {
-  }
+  selectedMilestone: string = '';
+
+  constructor(
+    private filteredService: FilteredService,
+    private MilestoneService: MilestoneService
+  ) {}
+  selectForeClosure(resultData: string) {}
   ngOnInit(): void {
     this.DilloadData();
+    this.MilestoneService.selectedMilestone$.subscribe((milestoneName) => {
+      this.selectedMilestone = milestoneName;
+    });
   }
   DilloadData(): void {
-    this.filteredService.getDilFilteredData(this.loanID, this.investorID, this.clientID, this.milestoneID).subscribe(
-      (data: any) => {
-        this.dils = data;
-      },
-      (error) => {
-        console.error('Error fetching Foreclosuretype types:', error);
-      }
-    );
+    this.filteredService
+      .getDilFilteredData(
+        this.loanID,
+        this.investorID,
+        this.clientID,
+        this.milestoneID
+      )
+      .subscribe(
+        (data: any) => {
+          this.dils = data;
+        },
+        (error) => {
+          console.error('Error fetching Foreclosuretype types:', error);
+        }
+      );
   }
 }
