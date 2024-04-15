06fc937930eb4b066b6cc956c80c1cd46a1e3ab7
Hepin <hepinponkiya@gmail.com>
2024-04-15 11:14:06 +0530

    Adding RouterLinks in Progresh Panel

--- src/app/Interceptor/add-token.interceptor.ts
+++ src/app/Interceptor/add-token.interceptor.ts
@@ -20,7 +20,7 @@ export class AddTokenInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<unknown>> {
     // Get the token from AuthService
     const token = this.authService.getToken();
-    // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJoZXBpbnBvbmtpeWFAZ21haWwuY29tIiwicGVybWlzc2lvbnMiOiJ1c2VyIiwibmFtZSI6IkhlcGluIFBvbmtpeWEiLCJpZCI6NywiZXhwIjoxNzEyOTQxNDMxfQ.QVkMmyQz-tHHbCJOMh-bDMVH5P-FWme4O0E2kDgJdQE';
+    // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJoZXBpbnBvbmtpeWFAZ21haWwuY29tIiwicGVybWlzc2lvbnMiOiJ1c2VyIiwibmFtZSI6IkhlcGluIFBvbmtpeWEiLCJpZCI6NywiZXhwIjoxNzEzMjAxNjE1fQ.v_Gk5TQJvF0h8dKNBWW0jWc_4-mRFU3YO56t4hjCXdg';
 
     // Clone the request and set the authorization header if token exists
     if (token) {
